b1b75dceae385404e6daddfcf8930b25bfe344fd
unknown <laurence@MHLT038.ad.nerc.ac.uk>
2015-10-05 22:31:24 +0100

    refactored home page view to use a template

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 from lists import views
 
 urlpatterns = [
-	url(r'^$', views.home_page, name='home'),
-	#url(r'^admin/', include(admin.site.urls)),
-	]
+    url(r'^$', views.home_page, name='home'),
+    #url(r'^admin/', include(admin.site.urls)),
+    ]
