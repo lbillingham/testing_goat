b992e89a0d8a5f9688ce710a79ba238e1e463759
unknown <laurence@MHLT038.ad.nerc.ac.uk>
2015-10-07 22:35:27 +0100

    func test kludgy works for 1 item, fails on second, now refactor

--- superlists/funtional_tests.py
+++ superlists/funtional_tests.py
@@ -4,7 +4,7 @@ import unittest
 
 class NewVisitorTest(unittest.TestCase):
     """
-    Test our onboarding poorocess for new visitors to our sitre
+    Test our onboarding process for new visitors to our sitre
     """
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -34,20 +34,24 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Buy peacock feathers')
 
         # When she hits <enter>, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list
+        # "1: Buy peacock feathers" as an item in a to-do list'
         inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-                any(row.text == '1: Buy peacock feathers' for row in rows),
-                'New to-do item did not appear in table'
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # There is still a textbox inviting her to add anoter item
         # She enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
         # Edith wonders whether the site will remember her list. Then she sees
         #  that it has generated a unique URL for her --- there is some
