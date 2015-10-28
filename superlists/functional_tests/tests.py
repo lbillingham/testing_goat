2fe167cb7709a59e392280a3fdb16f9a7d58a48b
unknown <laurence@MHLT038.ad.nerc.ac.uk>
2015-10-28 07:58:52 +0000

    made browser quit after 1st user's part of func tests run. removed spurious .pyc from git tracking

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -62,7 +62,7 @@ class NewVisitorTest(LiveServerTestCase):
         #
         ## We use a new browser session to make sure that none of Edith's information
         ## is comming through cookies etc.
-        #self.browser.quit()
+        self.browser.quit()
         self.browser = webdriver.Firefox()
 
         # Francis visits the home page... no sign of Edith's list
