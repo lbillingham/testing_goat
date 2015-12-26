f53e587bbf072ae6b9803f6db95546c8df9b2936
unknown <laurence@MHLT038.ad.nerc.ac.uk>
2015-12-26 19:37:01 +0000

    reorganized moved many things into ~/source/

--- source/superlists/settings.py
+++ source/superlists/settings.py
@@ -74,7 +74,6 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
