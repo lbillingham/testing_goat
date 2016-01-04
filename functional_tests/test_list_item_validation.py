e957d110605d8d8a01cb4c9a4ba54e3a91f2c697
Billingham <laurence@bgs.ac.uk>
2016-01-04 21:18:02 +0000

    fleshed out test for tiem validation as non-blank

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -7,15 +7,25 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_item(self):
         # Edith goes to the home page and accidentally tries to submit an empty
         # list item: she hits <Enter> on an empty text box.
-        #
+        self.browser.get(self.server_url)
+        self.browser.find_elements_by_id('id_new_item').send_keys('\n')
         # The home page refreshes, there is an error message saying that list
         # items cannot be blank.
-        #
+        error = self.browser.find_elements_by_css_selector('.has-error')
+        self.asserEqual(error.text, "You can't have an empty list item")
         # Edith tries again, this time providing text for the item.
         # This now works.
-        #
+        message = "I'm not nothing"
+        self.browser.find_elements_by_id('id_new_item').send_keys(message)
+        self.check_for_row_in_list_table('1: {}'.format(message))
+
         # Somewhat perversely, she decides to try to submit a second blank item
         # and recieves a similar warning on the list page.
-        #
-        # And Edith can correct things buyu filling some text in.
-        self.fail('test not yet written')
+        self.browser.find_elements_by_id('id_new_item').send_keys('\n')
+        self.asserEqual(error.text, "You can't have an empty list item")
+
+        # And Edith can correct things by filling some text in.
+        second_message = "I'm not nothing neither"
+        self.browser.find_elements_by_id('id_new_item').send_keys(second_message)
+        self.check_for_row_in_list_table('1: {}'.format(message))
+        self.check_for_row_in_list_table('1: {}'.format(second_message))
