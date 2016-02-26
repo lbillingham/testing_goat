b4a132e041922dd3cf2b8141885ec94a9a31422a
Billingham <laurence@bgs.ac.uk>
2016-02-26 19:15:06 +0000

    added `form.save()` method
    reduces duplication in the validation code
    end of chapter 11

--- lists/views.py
+++ lists/views.py
@@ -15,24 +15,15 @@ def view_list(request, list_id):
     if request.method == 'POST':
         form = ItemForm(data=request.POST)
         if form.is_valid():
-            Item.objects.create(text=request.POST['text'], list=list_)
+            form.save(for_list=list_)
             return redirect(list_)
-        try:
-            item = Item(text=request.POST['text'], list=list_)
-            item.full_clean()
-            item.save()
-            return redirect(list_)
-        except ValidationError:
-            error = "You can't have an empty list item"
-    return render(request, 'list.html', {
-        'list': list_, 'form': form, 'error': error
-    })
+    return render(request, 'list.html', {'list': list_, 'form': form})
 
 def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
         list_ = List.objects.create()
-        Item.objects.create(text=request.POST['text'], list=list_)
+        form.save(for_list=list_)
         return redirect(list_)
     else:
         return render(request, 'home.html', {'form': form})
