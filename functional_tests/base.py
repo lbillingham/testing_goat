580d26dc508aafb1e99cbf836322640764b71087
Billingham <laurence@bgs.ac.uk>
2016-05-04 22:07:43 +0100

    moved login checkers to base

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,6 +1,8 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 import sys
+
 
 class FunctionalTest(StaticLiveServerTestCase):
     """
@@ -21,17 +23,10 @@ class FunctionalTest(StaticLiveServerTestCase):
             super().tearDownClass()
 
     def setUp(self):
-        # profile = webdriver.FirefoxProfile()
-        # profile.set_preference("browser.startup.homepage", "about:blank");
-        # profile.set_preference("startup.homepage_welcome_url", "about:blank");
-        # profile.set_preference("startup.homepage_welcome_url.additional", "about:blank");
-        # self.browser = webdriver.Firefox(profile)
-
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
-        #self.browser.refresh()
         self.browser.quit()
 
     def check_for_row_in_list_table(self, row_text):
@@ -42,3 +37,21 @@ class FunctionalTest(StaticLiveServerTestCase):
     def get_item_input_box(self):
         box = self.browser.find_element_by_id('id_text')
         return box
+
+    def wait_for_element_with_id(self, element_id):
+        WebDriverWait(self.browser, timeout=30).until(
+            lambda b: b.find_element_by_id(element_id),
+            'Could not find elem with id {0}. Page text was\n{1}'.format(
+                element_id, self.browser.find_element_by_tag_name('body').text
+            )
+        )
+
+    def wait_to_be_logged_in(self, email):
+        self.wait_for_element_with_id('id_logout')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn(email, navbar.text)
+
+    def wait_to_be_logged_out(self, email):
+        self.wait_for_element_with_id('id_login')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn(email, navbar.text)
