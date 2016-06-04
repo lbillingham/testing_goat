c4be647f4ed82522693395c502553cc531386bf0
Billingham <laurence@bgs.ac.uk>
2016-06-04 23:12:17 +0100

    funct tests still dinnae run against staing
    hopefully this file twidddling malarky will sor it eventually

--- functional_tests/fabfile.py
+++ functional_tests/fabfile.py
@@ -1,23 +1,34 @@
 from fabric.api import env, run
+from os import path
+
+from server_tools import THIS_FOLDER
+KEY_FILE = path.join(THIS_FOLDER, 'key.f')
 
 
 def _get_base_folder(host):
     return '~/sites' + host
 
+
 def _get_manage_dot_py(host):
+    print('!!!!! fabfile._get_manage_dot_py !!!!!')
     return '{path}/virtualenv/bin/python {path}/source/manage.py'.format(
         path=_get_base_folder(host)
     )
 
+
 def reset_database():
+    print('**** fabfile.reset_database *****')
     run('{manage_py} flush --noinput'.format(
         manage_py=_get_manage_dot_py(env.host)
     ))
 
 
 def create_session_on_server(email):
+    print('### fabfile.create_session_on_server ####')
     session_key = run('{manage_py} create_session {email}'.format(
         manage_py=_get_manage_dot_py(env.host),
         email=email
     ))
     print(session_key)
+    with open(KEY_FILE, 'w') as file_:
+        file_.write(session_key)
--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -2,6 +2,7 @@ from os import path, sep
 import subprocess
 
 THIS_FOLDER = path.dirname(path.abspath(__file__))
+print(THIS_FOLDER)
 FAB_PATH = path.join(
     'c:', sep, 'Users', 'laurence', 'AppData', 'Local',
     'Continuum', 'Anaconda3', 'envs', 'fabric', 'Scripts',
@@ -10,15 +11,33 @@ FAB_PATH = path.join(
 
 
 def create_session_on_server(host, email):
-    return subprocess.check_output(
-        [
-            FAB_PATH,
-            'create_session_on_server:email={}'.format(email),
-            '--host={}'.format(host),
-            '--hide=everthing, status',
-        ],
-        cwd=THIS_FOLDER
-    ).decode().strip()
+    from fabfile import KEY_FILE
+    # session_key = subprocess.check_output(
+    #     [
+    #         FAB_PATH,
+    #         'create_session_on_server:email={}'.format(email),
+    #         '--host={}'.format(host),
+    #         '--hide=everthing, status',
+    #     ],
+    #     cwd=THIS_FOLDER
+    # ).decode().strip()
+    max_wait = 180
+    waited = 0
+    step = 1
+
+    print('$$$$$$ server_tools.create_session_on_server $$$$$$')
+    while (not os.path.exists(file_path)) and waited < max_wait:
+        time.sleep(step)
+        waited += step
+        print('waiting')
+
+    if os.path.isfile(file_path):
+        with open(KEY_FILE, 'r') as file_:
+            file_.read(session_key)
+            session_key = session_key.decode().strip()
+    else:
+        raise ValueError('{} isn\'t a file!'.format(file_path))
+    return session_key
 
 
 def reset_database(host):
