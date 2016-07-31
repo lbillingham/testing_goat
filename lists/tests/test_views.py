419fff2fbe45320a9faee3dc8a9edc7fb27244bd
Billingham <laurence@bgs.ac.uk>
2016-07-31 16:42:03 +0100

    ugly mocky test for my_lists ch19

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -5,6 +5,7 @@ from django.template.loader import render_to_string
 from django.test import TestCase
 from django.utils.html import escape
 from unittest import skip
+from unittest.mock import Mock, patch
 
 from lists.forms import (
     ItemForm, ExistingListItemForm,
@@ -83,13 +84,22 @@ class NewListTest(TestCase):
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
 
-    def test_list_owner_is_saved_if_user_is_authenticated(self):
+    @patch('lists.views.List')
+    def test_list_owner_is_saved_if_user_is_authenticated(self, mockList):
+        mock_list = List.objects.create()
+        mock_list.save = Mock()
+        mockList.return_value = mock_list
         request = HttpRequest()
-        request.user = User.objects.create(email='a@b.com')
+        request.user = User.objects.create()
         request.POST['text'] = 'new list item'
+
+        def check_owner_assigned():
+            self.assertEqual(mock_list.owner, request.user)
+
+        mock_list.save.side_effect = check_owner_assigned
+
         new_list(request)
-        list_ = List.objects.first()
-        self.assertEqual(list_.owner, request.user)
+        mock_list.save.assert_called_once_with()
 
 
 class ListViewTest(TestCase):
