1f0efd0ab65e0f806258700d74ef428a0d024dbf
Billingham <laurence@bgs.ac.uk>
2016-08-02 23:15:34 +0100

    whitespace fixed up

--- lists/forms.py
+++ lists/forms.py
@@ -6,6 +6,7 @@ from lists.models import Item
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
 DUPLICATE_ITEM_ERROR = "You've already got this item in your list"
 
+
 class ItemForm(forms.models.ModelForm):
 
     class Meta:
